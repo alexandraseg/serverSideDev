1e2c1a70008d495c851afdd984fc9fe773bdb1e3
alexandraseg <alex.segkou@gmail.com>
2021-08-14 23:51:15 +0300

    Express REST API with Mongoose Part One

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -11,6 +11,21 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
+//new code - start
+
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
+
+//new code - end
+
 var app = express();
 
 // view engine setup
