cd6408f9d09b7b4d42cc3d0784e6fcdd0c0a4ff1
alexandraseg <alex.segkou@gmail.com>
2021-08-28 21:47:49 +0300

    File Upload

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -10,6 +10,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
@@ -65,6 +66,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
