b9661aca15a06fff2c11fa0d392aca84b9fb7585
alexandraseg <alex.segkou@gmail.com>
2021-09-06 19:22:27 +0300

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId: '197397442330042',
+        clientSecret: '355e9a321f978574102a8c39317f425f'
+    }
 }
